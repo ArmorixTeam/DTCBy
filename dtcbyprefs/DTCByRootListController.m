509af85ebb6cd37844e773ca66fe126d423cd928
Zodac <devblueb@gmail.com>
2026-01-05 12:57:23 -0500

    Implement DTCByRootListController with basic functions

---
+++ dtcbyprefs/DTCByRootListController.m
@@ -0,0 +1,37 @@
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+
+@interface DTCByRootListController : PSListController
+@end
+
+@implementation DTCByRootListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    }
+    return _specifiers;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Set tint color
+    self.view.tintColor = [UIColor colorWithRed:0.1 green:0.5 blue:1.0 alpha:1.0];
+    if (self.navigationController) {
+        self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.1 green:0.5 blue:1.0 alpha:1.0];
+    }
+}
+
+- (void)openGitHub {
+    NSURL *url = [NSURL URLWithString:@"https://github.com/ZodaciOS"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
+
+- (void)respring {
+    pid_t pid;
+    const char *args[] = {"/var/jb/usr/bin/sbreload", NULL};
+    posix_spawn(&pid, args[0], NULL, NULL, (char *const *)args, NULL);
+}
+
+@end
