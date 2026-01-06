844990bfc6a26608a42e01edaec2b806283e49e8
Zodac <devblueb@gmail.com>
2026-01-05 19:17:27 -0500

    Update DTCByRootListController.m

--- dtcbyprefs/DTCByRootListController.m
+++ dtcbyprefs/DTCByRootListController.m
@@ -2,30 +2,25 @@
 #import <Preferences/PSListController.h>
 #import <spawn.h>
 
-@interface PSListController : UIViewController
-@property (nonatomic, retain) NSArray *specifiers;
-- (NSArray *)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
-@end
-
 @interface DTCByRootListController : PSListController
 @end
 
 @implementation DTCByRootListController
 
 - (NSArray *)specifiers {
-    if (!self.specifiers) {
-        self.specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
-    return self.specifiers;
+    return _specifiers;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Set tint color
+
     self.view.tintColor = [UIColor colorWithRed:0.1 green:0.5 blue:1.0 alpha:1.0];
     if (self.navigationController) {
-        self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.1 green:0.5 blue:1.0 alpha:1.0];
+        self.navigationController.navigationBar.tintColor =
+            [UIColor colorWithRed:0.1 green:0.5 blue:1.0 alpha:1.0];
     }
 }
 
