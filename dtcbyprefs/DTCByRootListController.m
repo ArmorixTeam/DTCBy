fa91eff73d9b80c4d90da973beef6dca8659c902
Zodac <devblueb@gmail.com>
2026-01-05 19:45:28 -0500

    Refactor specifiers method for clarity

--- dtcbyprefs/DTCByRootListController.m
+++ dtcbyprefs/DTCByRootListController.m
@@ -8,10 +8,10 @@
 @implementation DTCByRootListController
 
 - (NSArray *)specifiers {
-    if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    if (!self.specifiers) {
+        [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
-    return _specifiers;
+    return self.specifiers;
 }
 
 - (void)viewDidLoad {
